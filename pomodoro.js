41f9f3663fa84afaee793dc4e7672735e211350c
Ion Gorincioi <gorincioii@yahoo.com>
2016-07-19 21:19:13 -0700

    Work on functionality

---
+++ pomodoro.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+  var minutes = 25;
+  var isPaused = false;
+  var timerId = 0;
+
+  $("#length").text(minutes);
+  function startTimer(duration, display) {
+      var timer = duration, minutes, seconds;
+      console.log("timer", timer);
+      timerId = setInterval(function () {
+
+        if(!isPaused){
+          minutes = parseInt(timer / 60);
+          seconds = parseInt(timer % 60);
+
+          minutes = minutes < 10 ? "0" + minutes : minutes;
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          display.text(minutes + ":" + seconds);
+          timer--;
+          console.log(timer);
+          if (timer < 0) {
+              timer = duration;
+              $("#stop").hide();
+              $("#resume").hide();
+              clearInterval(timerId);
+          }
+
+        }
+
+      }, 1000);
+    console.log("id", timerId);
+
+  }
+
+
+  function startPomodoro(min){
+
+    var minute = 60 * min,
+    display = $('#time');
+    startTimer(minute, display);
+  }
+
+  // Event listeners
+
+  // Session length actions
+
+  $("#minus").on("click", function(){
+    if(minutes > 1){
+      minutes -= 1;
+      $("#length").text(minutes);
+      $("#time").text(minutes + ":00");
+    }
+
+  });
+
+  $("#plus").on("click", function(){
+    if( minutes < 45){
+      minutes += 1;
+      $("#length").text(minutes);
+      $("#time").text(minutes + ":00");
+    }
+
+  });
+
+  // Start button
+  $("#start").on("click", function(){
+    $("#minus, #plus").attr("disabled", true);
+
+    $(this).hide();
+    isPaused = false;
+    startPomodoro(minutes);
+    $("#stop").show();
+
+  });
+
+  //Stop button
+  $("#stop").on("click", function(){
+    $(this).hide();
+    $("#resume").show();
+    isPaused = !isPaused;
+  });
+
+  //Resume button
+  $("#resume").on("click", function(){
+    $(this).hide();
+    $("#stop").show();
+    isPaused = !isPaused;
+
+  });
+
+  //Reset button
+  $("#reset").on("click", function(){
+    $("#minus, #plus").attr("disabled", false);
+
+
+    $("#stop").hide();
+    $("#resume").hide();
+    $("#start").show();
+    clearInterval(timerId);
+    $("#time").text(minutes + ":00");
+
+
+  });
+})
